0ba057c54e48a1031bc4c00b7aa86ae1475f81ba
karimDevWM <ka.trabelsi9@gmail.com>
2022-07-08 13:26:27 +0200

    add a new project entry in the project list

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,4 +1,5 @@
 import flask_react from '../assets/flask_react.png';
+import asp_net_core_angular from '../assets/ASP.net core_Angular.png';
 
 
 // list of objects creation
@@ -8,5 +9,11 @@ export const ProjectList = [
         image: flask_react,
         skills : "React JS, Python, SQLAlchemy, Mysql",
         github_url : "https://github.com/karimDevWM/karim_portfolio",
-    }
+    },
+    {
+        name : "ASP.NET Core web API - Angular Blog",
+        image : asp_net_core_angular,
+        skills : "ASP.Net core, Angular, API, Swagger, SQL server, Sql Server Management Studio, Visual studio",
+        github_url : "https://github.com/karimDevWM/DevBlog_Full_Stack_ASP.Net_Web_API_Angular",
+    },
 ]
