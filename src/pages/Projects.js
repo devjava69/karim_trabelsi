ad781020dff27917f9928ea04c71dcf614415852
karimDevWM <ka.trabelsi9@gmail.com>
2022-07-08 13:27:00 +0200

    add bootstrap disposal for projects cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,17 +3,21 @@ import ProjectItem from '../components/ProjectItem';
 import '../styles/Projects.css'
 import { ProjectList } from '../helpers/ProjectList'
 
+
 function Projects() {
   return (
-    <div className='projects'>
-      <h1>My Personal Projects</h1>
-      <div className='projectList'>
-        { ProjectList.map((project, index)=>{
-          return <ProjectItem id={index} name={project.name} image={project.image}/>
-        })}
+  <div className='row'>
+    <div className='col-sm col-md col-lg-3'>
+      <div className='projects'>
+        <h1>My Personal Projects</h1>
+        <div className='projectList'>
+          { ProjectList.map((project, index)=>{
+            return <ProjectItem id={index} name={project.name} image={project.image}/>
+          })}
       </div>
     </div>
+    </div>
+  </div>
   )
 }
-
 export default Projects
